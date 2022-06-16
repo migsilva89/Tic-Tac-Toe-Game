b3dfe0154428c6e4ecb4dfe1edeb2a7304c4ed8d
migsilva89 <migdrum@gmail.com>
2022-06-17 01:48:06 +0200

    design

--- app.js
+++ app.js
@@ -14,41 +14,46 @@ const winningOptions = [
     [3,5,7],
 ]
 
-function checkResult() {
+function checkResult(player) {
+
+    let res1 = winningOptions.filter(combo => combo.filter(comboValue => playerOChoices.indexOf(comboValue) > -1).length == 3);
+    let resFinal1 = [].concat.apply([], res1)
     
-    let res = winningOptions.filter(combo => combo.filter(comboValue => playerOChoices.indexOf(comboValue) > -1).length == 3);
-    var resFinal = [].concat.apply([], res)
+    let res2 = winningOptions.filter(combo => combo.filter(comboValue => playerXChoices.indexOf(comboValue) > -1).length == 3);
+    let resFinal2 = [].concat.apply([], res2)
 
     winningOptions.forEach(element => {
-        if (JSON.stringify(resFinal) == JSON.stringify(element)){
-            console.log("ganhei")
-        } 
+        if (JSON.stringify(resFinal1) == JSON.stringify(element)){
+            alert(`${player} WINS!`)
+        } else if (JSON.stringify(resFinal2) == JSON.stringify(element)) {
+            alert(`${player} WINS!`)
+        }
     });
 }
 
 function getBtnNumber(btnNumber) {
 
     const firstBtn = document.getElementById(`bt${btnNumber}`)
-    
-    //AQUI BLOQUEAMOS JOGAR NO MESMO BTN
+    let player = currentPlayer
+
     if(firstBtn.innerText != "") return;
 
-    //AQUI INNER TEXT TOMA VALOR DE CUURENT PLAYER
-    firstBtn.innerText = currentPlayer;
+    
 
     if(currentPlayer == "O") {
+        firstBtn.innerText = currentPlayer;//aqui leva o design do X
+        firstBtn.innerHTML = `<img src="./images/O.png" alt="">`
         currentPlayer = "X";
         playerOChoices.push(btnNumber)
     }  
     else {
+        firstBtn.innerText = currentPlayer;//aqui leva o design na roda
+        firstBtn.innerHTML = `<img class="w-20 h-24" src="./images/x.png" alt="">`
         currentPlayer = "O"; 
         playerXChoices.push(btnNumber)
     }
 
-    console.log(playerOChoices)
-    console.log(playerXChoices)
-    
-    checkResult()
+    checkResult(player)
 
 }
 
