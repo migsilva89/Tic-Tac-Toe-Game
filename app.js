49f94e732ed5f90f0860586a5993eafba5bfafcb
migsilva89 <migdrum@gmail.com>
2022-06-17 14:30:20 +0200

    reset function + getplayer result function + fix check result

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
-const playerXChoices = []
-const playerOChoices = []
-var currentPlayer = "O"
+let playerXChoices = []
+let playerOChoices = []
+let currentPlayer = "O"
 
 const winningOptions = [
     [1,2,3],
@@ -14,45 +14,80 @@ const winningOptions = [
     [3,5,7],
 ]
 
-function checkResult(player) {
-
-    let res1 = winningOptions.filter(combo => combo.filter(comboValue => playerOChoices.indexOf(comboValue) > -1).length == 3);
-    let resFinal1 = [].concat.apply([], res1)
+function checkResult(player) 
+{
     
-    let res2 = winningOptions.filter(combo => combo.filter(comboValue => playerXChoices.indexOf(comboValue) > -1).length == 3);
-    let resFinal2 = [].concat.apply([], res2)
+    let playerOneMoves = getPlayerResult(playerOChoices);
+    let playerTwoMoves = getPlayerResult(playerXChoices);
 
     winningOptions.forEach(element => {
-        if (JSON.stringify(resFinal1) == JSON.stringify(element)){
-            alert(`${player} WINS!`)
-        } else if (JSON.stringify(resFinal2) == JSON.stringify(element)) {
-            alert(`${player} WINS!`)
+
+        if (JSON.stringify(playerOneMoves) == JSON.stringify(element)){
+            resetBoard(player);
+            return;
+        }
+
+        if (JSON.stringify(playerTwoMoves) == JSON.stringify(element)) {
+            resetBoard(player);
+            return;
         }
     });
+
 }
 
-function getBtnNumber(btnNumber) {
+function resetBoard(player = null)
+{
+    currentPlayer = "0";
+    playerXChoices = [];
+    playerOChoices = [];
+    document.querySelectorAll(".choice").forEach(boardSlot => boardSlot.innerHTML = '');
+    
+    // If we have a player that actuall own the game, then show the div yat! :p
+    if(player !== null){
+        document.getElementById('winner').innerHTML = '🎉 Yay! Player '+player+' Wins!!'
+    }
+    
+    // Reset the winner div after 2 seconds ( 2 x 1000 ). We dont need the myTimeout variable here
+    // But this is very important in vue and react because we want to clear any pending "effects"
+    // Before unmounting the component.
+    const myTimeout = setTimeout(() => document.getElementById('winner').innerHTML = '' , 2000);
+}
 
-    const firstBtn = document.getElementById(`bt${btnNumber}`)
-    let player = currentPlayer
 
-    if(firstBtn.innerHTML != "") return;
+function getPlayerResult(currentPlayerMoves)
+{ 
+    let filterPlayerMoves = winningOptions.filter(combo => combo.filter(comboValue => currentPlayerMoves.indexOf(comboValue) > -1).length == 3);
+    return [].concat.apply([], filterPlayerMoves);
+}
 
+function toggleSlot(btnNumber) {
+
+    const slotClicked = document.getElementById(`bt${btnNumber}`)
+
+    let currentPreviousPlayer = currentPlayer
+
+    // The slot is already filled, dont cheat please
+    if(slotClicked.innerHTML != ""){
+        return;
+    }
+    
+    // Player O is playing, mark with a 0 and then switch to the next player
     if(currentPlayer == "O") {
-        firstBtn.innerText = currentPlayer;//aqui leva o design do X
-        firstBtn.innerHTML = `<img src="./images/O.png" alt="">`
+        slotClicked.innerText = currentPlayer; //aqui leva o design do X
+        slotClicked.innerHTML = `<img src="./images/O.png" alt="">`
         currentPlayer = "X";
         playerOChoices.push(btnNumber)
-    }  
+    }
 
+     // Player X is playing, mark with a X and then switch to the next player
     else {
-        firstBtn.innerText = currentPlayer;//aqui leva o design na roda
-        firstBtn.innerHTML = `<img class="w-20 h-24" src="./images/x.png" alt="">`
+        slotClicked.innerText = currentPlayer;//aqui leva o design na roda
+        slotClicked.innerHTML = `<img class="w-20 h-24" src="./images/x.png" alt="">`
         currentPlayer = "O"; 
         playerXChoices.push(btnNumber)
     }
-
-    checkResult(player)
+   
+    checkResult(currentPreviousPlayer)
 
 }
 
